6caed93cfa94765107de9b460da0cf185d65c088
Vinicius <viniciuz.com>
2024-04-25 16:43:50 +0100

    add nova pasta

--- build/scripts/sum.js
+++ build/scripts/sum.js
@@ -1,1 +1,1 @@
-function ಠ_ಠ607(o,n){return o+n}console.log(ಠ_ಠ607(10,10));
+function ಠ_ಠ607(n,o){return n+o}console.log(ಠ_ಠ607(10,10));
--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,33 @@
 const gulp = require('gulp');
-const sass = require('gulp-sass') (require('sass'));
+const sass = require('gulp-sass')(require('sass'));
 const sourcemaps = require('gulp-sourcemaps');
 const uglify = require('gulp-uglify');
 const obfuscate = require('gulp-obfuscate');
 const imagemin = require('gulp-imagemin');
 
-function comprimeimg() {
-    return gulp.src('./source/images/*')
+function comprimirImagens() {
+    return gulp.src('./source/images/')
         .pipe(imagemin())
         .pipe(gulp.dest('./build/images'));
 }
 
 function comprimirJS() {
     return gulp.src('./source/scripts/*.js')
-    .pipe(uglify())
-    .pipe(obfuscate())
-    .pipe(gulp.dest('./build/scripts'))
+        .pipe(uglify())
+        .pipe(obfuscate())
+        .pipe(gulp.dest('./build/scripts'));
 }
 
-function compilaSass() {
+function compilarSass() {
     return gulp.src('./source/styles/main.scss')
-    .pipe(sourcemaps.init())
-    .pipe(sass({
-        outputStyle:'compressed'
-    }))
-    .pipe(sourcemaps.write('./maps'))
-    .pipe(gulp.dest('./build/styles'));
+        .pipe(sourcemaps.init())
+        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+        .pipe(sourcemaps.write('./maps'))
+        .pipe(gulp.dest('./build/styles'));
 }
 
-
-
-
-
-exports.default = function(){
-    gulp.watch('./source/images/*', {ignoreInitial: false}, gulp.series(comprimeimg));
-    gulp.watch('./source/scripts/*.js', {ignoreInitial: false}, gulp.series(comprimirJS));
-    gulp.watch('./source/styles/*.scss', {ignoreInitial: false}, gulp.series(compilaSass));
-    
-}
+exports.default = function() {
+    gulp.watch('./source/images/*', { ignoreInitial: false }, gulp.series(comprimirImagens));
+    gulp.watch('./source/scripts/*.js', { ignoreInitial: false }, gulp.series(comprimirJS));
+    gulp.watch('./source/styles/*.scss', { ignoreInitial: false }, gulp.series(compilarSass));
+};
